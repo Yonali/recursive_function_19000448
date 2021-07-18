4605a1116c5e2d135b3cc3672ee89437b073ca7f
Yonali <fernandoymadhushika@gmail.com>
2021-07-18 18:56:09 +0000

    q6_answer

--- Q6/Q6.scala
+++ Q6/Q6.scala
@@ -1,13 +1,21 @@
-object Q6 extends App{
+import java.util.Scanner;
+import scala.io.StdIn.readInt;
 
+object Q6{
   def f(n:Int):Int=n match{
   case n if n==0 => 0
   case n if n==1 => 1
   case n => f(n-1)+f(n-2)
 
   }
+  def main(args:Array[String]){
+    var input = new Scanner(System.in);
+    println("Enter a number");
+    val N=input.nextInt();
 
-println(f(5))
+    println(f(N))
+  }  
+
 
 }
 
