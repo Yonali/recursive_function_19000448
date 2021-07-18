8cde6d954635a382907766e09dc1aa9600d713ec
Yonali <fernandoymadhushika@gmail.com>
2021-07-18 18:22:27 +0000

    q1_answer

--- Q1/Q1.scala
+++ Q1/Q1.scala
@@ -1,22 +1,23 @@
+import java.util.Scanner;
+import scala.io.StdIn.readInt;
 
-
-
-
-
-
-
-
-
-
-
-object Q1 extends App{
-  def prime(x:Int):Boolean={
-    if(x<=1)
-      false
-    else if(x==2)
-      true
-    else
-      !(2 to (x-1)).exists(i=>x % i==0)
+object Q1{
+  def gcd(l:Int,k:Int):Int=k match{
+    case 0=>l
+    case i if (i>l) => gcd(i,l)
+    case _ => gcd(k,l%k)
   }
-  println(prime(3));
+  def prime(x:Int,n:Int=2):Boolean=n match{
+    case i if(x==i) => true
+    case i if (gcd(x,i)>1) => false
+    case i => prime(x,i+1)
+  }
+  def main(args:Array[String]){
+    var input = new Scanner(System.in)
+    println("Enter a number")
+    val N=input.nextInt();
+
+    print(prime(N))
+  }  
+
 }
