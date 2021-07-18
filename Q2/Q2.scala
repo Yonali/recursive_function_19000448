db9305a0bdc7c758fdbbade7a94e95603933cb3f
Yonali <fernandoymadhushika@gmail.com>
2021-07-18 18:41:39 +0000

    q2_answer' :q cd

--- Q2/Q2.scala
+++ Q2/Q2.scala
@@ -1,13 +1,32 @@
-object Q2 extends App{
-  def primeSeq(x:Int) : Boolean = {
-    if(x<=1)
-      false
-    else if(x==2)
-      true
-    else
-      !(2 to (x-1)).exists(i=>x % i==0)
-  }
+import java.util.Scanner;
+import scala.io.StdIn.readInt;
 
-  (1 to 10).foreach(x=> if (primeSeq(x)) println("%d is a prime number".format(x)))
+object Q2{
+  def gcd(l:Int,k:Int):Int=k match{
+    case 0 => l
+    case i if (i>l) => gcd(i,l)
+   case _ => gcd(k,l%k)
+  }
+  def prime(x:Int, y:Int=2): Boolean = y match{
+    case i if(i==x) => true
+    case i if gcd(x,i) > 1 => false
+    case i => prime(x,i+1)
+  }
+  def primeSeq(p:Int) : Unit = {
+    if(p>2)
+      primeSeq(p-1);
+    var bool = (prime(p));
+    if(bool)
+      println(p);
+  }
+    
+  def main(args:Array[String]){
+    var input = new Scanner (System.in);
+    println("Enter a number")
+    val N=input.nextInt();
+
+    primeSeq(N);
+  }
+  
 }
 
